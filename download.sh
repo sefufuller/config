edaa53a8217df9d3a497eb9f4a09935fc3b86e01
sefufuller <sefu@outlook.com>
2018-12-10 19:45:59 -0500

    Update download.sh

--- download.sh
+++ download.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
 
-curl https://raw.githubusercontent.com/sefufuller/config/master/config.sh > config.sh
-source ./config.sh
+# curl https://raw.githubusercontent.com/sefufuller/config/master/config.sh > config.sh
+#source ./config.sh
+curl -O https://prerelease.keybase.io/keybase_amd64.deb
+sudo dpkg -i keybase_amd64.deb
+sudo apt-get install -f
+run_keybase
